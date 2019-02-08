650df18180070bbdcef31621b27aa0577b1f2aff
Naumenko Nikita <naumenkoniki@gmail.com>
2019-02-08 17:13:54 +0300

    Init commit

---
+++ lib/hexlet/oop.rb
@@ -0,0 +1,8 @@
+require "hexlet/oop/version"
+
+module Hexlet
+  module Oop
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/hexlet/oop/version.rb
@@ -0,0 +1,5 @@
+module Hexlet
+  module Oop
+    VERSION = "0.1.0"
+  end
+end
