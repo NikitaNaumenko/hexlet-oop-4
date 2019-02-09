f9f4faacd385ceb0d4c663eda3f1bb26a1dd7ec0
Naumenko Nikita <naumenkoniki@gmail.com>
2019-02-09 13:37:53 +0300

    Refactoring

--- lib/hexlet/oop.rb
+++ lib/hexlet/oop.rb
@@ -9,7 +9,13 @@ module Hexlet
     API_URL = 'http://ip-api.com/json/'.freeze
 
     class GeoIp
-      def get_info(ip_address:, client: Net::HTTP)
+      def initialize(client: Net::HTTP)
+        @client = client
+      end
+
+      attr_reader :client
+
+      def get_info(ip_address:)
         url = URI(API_URL + ip_address)
         result = client.get(url)
         JSON.parse(result)
--- spec/hexlet/oop_spec.rb
+++ spec/hexlet/oop_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe Hexlet::Oop do
   context 'return info' do
     it 'return info' do
       expected_response = { city: 'Penza', country: 'Russia', regionName: 'Penzenskaya Oblast' }.to_json
-      geo_ip = Hexlet::Oop::GeoIp.new
-      result = geo_ip.get_info(ip_address: '1.2.3.4', client: FakeHttpClient)
+      geo_ip = Hexlet::Oop::GeoIp.new(client: FakeHttpClient)
+      result = geo_ip.get_info(ip_address: '1.2.3.4')
       is_asserted_by { JSON.parse(expected_response) == result }
     end
   end
